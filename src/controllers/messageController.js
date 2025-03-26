7f1d9f9d36f9e1b76ecd607ad1f4f1d6f20b4e32
Himanshu Ramteke <himanshuramteke007@gmail.com>
2025-03-26 11:50:40 +0530

    Added presigned logic

--- src/controllers/messageController.js
+++ src/controllers/messageController.js
@@ -1,5 +1,7 @@
 import { StatusCodes } from "http-status-codes";
 
+import { s3 } from "../config/awsConfig.js";
+import { AWS_BUCKET_NAME } from "../config/serverConfig.js";
 import { getMessagesService } from "../services/messageService.js";
 import { customErrorResponse, internalErrorResponse, successResponse } from "../utils/common/responseObjects.js";
 
@@ -31,3 +33,26 @@ export const getMessages = async (req, res) => {
            .json(internalErrorResponse(error));
     };
 };
+
+export const getPresignedUrlFromAWS = async (req, res) => {
+    try {
+        const url = await s3.getSignedUrlPromise('putObject', {
+            Bucket: AWS_BUCKET_NAME,
+            Key: `${Date.now()}`,
+            Expires: 60 //1minute
+        });
+        return res
+            .status(StatusCodes.OK)
+            .json(successResponse(url, 'Presigned URL generated successfully'));
+    } catch (error) {
+        console.log('Error in getPresignedUrlFromAWS', error);
+        
+        if(error.statusCode) {
+            return res.status(error.statusCode).json(customErrorResponse(error));
+        }
+
+        return res 
+           .status(StatusCodes.INTERNAL_SERVER_ERROR)
+           .json(internalErrorResponse(error));
+    }
+}
